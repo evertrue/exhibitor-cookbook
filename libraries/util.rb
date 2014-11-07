e0766c53b8415b35a03e9ce16a5bc7ece2ca9605
Jeff Byrnes <jeff@evertrue.com>
2014-11-07 14:29:04 -0500

    RuboCop cleanup of Exhibitor::Util
    * Drop unnecessary `return` throughout
    * s/exists?/exist?/
    * Consistent, more readable spaces around braces & commas
    * Use underscore notation for unused vars inside blocks
    * Drop unnecessary `to_s` methods inside interpolated strings

--- libraries/util.rb
+++ libraries/util.rb
@@ -3,29 +3,29 @@
 module Exhibitor
   module Util
     def should_install_gradle?
-      return !Dir.exists?(File.join(Chef::Config[:file_cache_path],
-                                    "gradle-#{node[:gradle][:version]}"))
+      !Dir.exist?(File.join(Chef::Config[:file_cache_path],
+                            "gradle-#{node[:gradle][:version]}"))
     end
 
     def should_install_exhibitor?(jar_dest)
-      return !File.exists?(jar_dest)
+      !File.exist?(jar_dest)
     end
 
-    def render_properties_file(config={})
-      config.sort_by {|k,v| k}.collect do |k, v|
+    def render_properties_file(config = {})
+      config.sort_by { |k, _v| k }.collect do |k, v|
         "#{k.gsub('_', '-')}=#{v}"
       end.join("\n")
     end
 
     def render_s3_credentials(config)
-      config.sort_by {|k,v| k}.collect do |name, val|
+      config.sort_by { |k, _v| k }.collect do |name, val|
         "com.netflix.exhibitor.s3.#{name.gsub('_', '-')}=#{val}"
       end.join("\n")
     end
 
     def format_cli_options(opts)
-      opts.sort_by {|k,v| k}.collect do |opt, val|
-        "--#{opt.to_s} #{val.to_s}"
+      opts.sort_by { |k, _v| k }.collect do |opt, val|
+        "--#{opt} #{val}"
       end.join(' ')
     end
   end
