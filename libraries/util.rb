f8844be751b664c0210c4a4fc0f369201d9fbf0f
Jeff Byrnes <jeff@evertrue.com>
2015-10-05 17:02:33 -0400

    Rubocop: Use tr instead of gsub for a single char
    A total micro-optimization, but may as well while Rubocop is complaining

--- libraries/util.rb
+++ libraries/util.rb
@@ -13,13 +13,13 @@ module Exhibitor
 
     def render_properties_file(config = {})
       config.sort_by { |k, _v| k }.collect do |k, v|
-        "#{k.gsub('_', '-')}=#{v}"
+        "#{k.tr('_', '-')}=#{v}"
       end.join("\n")
     end
 
     def render_s3_credentials(config)
       config.sort_by { |k, _v| k }.collect do |name, val|
-        "com.netflix.exhibitor.s3.#{name.gsub('_', '-')}=#{val}"
+        "com.netflix.exhibitor.s3.#{name.tr('_', '-')}=#{val}"
       end.join("\n")
     end
 
