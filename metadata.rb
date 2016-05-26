b850b5ee8050e8e9ce42269b257fe294ad0cb4b0
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-05-26 15:57:00 -0400

    Add (and update) Supermarket URLs

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,9 @@ license          'Apache v2.0'
 description      'Installs Netflix Exhibitor'
 version          '0.10.0'
 
+issues_url 'https://github.com/evertrue/exhibitor-cookbook/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/evertrue/exhibitor-cookbook/' if respond_to?(:source_url)
+
 depends          'build-essential', '~> 2.2'
 depends          'java', '~> 1.35'
 depends          'runit', '~> 1.7'
