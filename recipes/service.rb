000526c6d9e65da91cb9bb50dc01194847be43ce
Tyler Marshall <tyler.marshall@rallyhealth.com>
2016-06-06 16:12:52 -0400

    Include Exhibitor::Util in the proper Chef Scope

--- recipes/service.rb
+++ recipes/service.rb
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-class Chef::Resource
+class Chef::Recipe
   include Exhibitor::Util
 end
 
