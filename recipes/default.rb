33317820d353df7dbd3d0e0898b2099490f918f8
Jeff Byrnes <thejeffbyrnes@gmail.com>
2016-07-12 14:29:57 -0400

    Fix keys for additional Exhibitor/ZK configs
    Fixes #39

--- recipes/default.rb
+++ recipes/default.rb
@@ -81,10 +81,12 @@ end
 
 # Write these values out as late as possible since their values can change.
 node.default['exhibitor']['config'].merge!(
-  'log_index_directory' => node['exhibitor']['log_index_dir'],
-  'zookeeper_log_directory' => node['zookeeper']['config']['dataLogDir'] || node['exhibitor']['transaction_dir'],
-  'zookeeper_data_directory' => node['zookeeper']['config']['dataDir'] || node['exhibitor']['snapshot_dir'],
-  'zookeeper_install_directory' => "#{node['zookeeper']['install_dir']}/*"
+  'log-index-directory' => node['exhibitor']['log_index_dir'],
+  'zookeeper-log-directory' => node['zookeeper']['config']['dataLogDir'] ||
+                               node['exhibitor']['transaction_dir'],
+  'zookeeper-data-directory' => node['zookeeper']['config']['dataDir'] ||
+                                node['exhibitor']['snapshot_dir'],
+  'zookeeper-install-directory' => "#{node['zookeeper']['install_dir']}/*"
 )
 
 file node['exhibitor']['cli']['defaultconfig'] do
