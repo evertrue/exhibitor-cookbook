76b97ad86868a95861c5bfdc5c77c195b0b7cb37
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-10-09 16:51:29 -0400

    Add supports metadata

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,11 @@ chef_version     '>= 12.5' if respond_to?(:chef_version)
 issues_url 'https://github.com/evertrue/exhibitor-cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/evertrue/exhibitor-cookbook/' if respond_to?(:source_url)
 
+supports 'ubuntu', '= 12.04'
+supports 'ubuntu', '= 14.04'
+supports 'ubuntu', '= 16.04'
+supports 'centos', '~> 6'
+
 depends          'build-essential'
 depends          'java', '~> 1.35'
 depends          'runit', '~> 3.0'
