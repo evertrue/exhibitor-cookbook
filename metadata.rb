82c45ee17d1a69403dffb4675630d216bf3f57c0
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-10-09 16:44:36 -0400

    FC066: Ensure chef_version is set

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache-2.0'
 description      'Installs Netflix Exhibitor'
 version          '4.1.0'
+chef_version     '>= 12.5' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/evertrue/exhibitor-cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/evertrue/exhibitor-cookbook/' if respond_to?(:source_url)
