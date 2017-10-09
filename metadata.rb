8ad461c2635bd14d90dad12d18f8a9c4066490b2
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-10-09 17:16:13 -0400

    Bump to v4.2.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'EverTrue'
 maintainer_email 'devops@evertrue.com'
 license          'Apache-2.0'
 description      'Installs Netflix Exhibitor'
-version          '4.1.0'
+version          '4.2.0'
 chef_version     '>= 12.5' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/evertrue/exhibitor-cookbook/issues' if respond_to?(:issues_url)
