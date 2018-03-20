efc53cd0bccb3ece89382344e623d8d8c51bfd6f
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-03-20 11:29:09 -0400

    Drop support for Ubuntu 12.04

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,6 @@ chef_version     '>= 12.5' if respond_to?(:chef_version)
 issues_url 'https://github.com/evertrue/exhibitor-cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/evertrue/exhibitor-cookbook/' if respond_to?(:source_url)
 
-supports 'ubuntu', '= 12.04'
 supports 'ubuntu', '= 14.04'
 supports 'ubuntu', '= 16.04'
 supports 'centos', '~> 6.0'
