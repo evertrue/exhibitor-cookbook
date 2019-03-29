2f918c3d3b0092ebf425a2abe3097654ece30db7
John Maguire <john.maguire@slicelife.com>
2019-03-29 14:13:31 +0000

    update jcenter URL

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'EverTrue'
 maintainer_email 'devops@evertrue.com'
 license          'Apache-2.0'
 description      'Installs Netflix Exhibitor'
-version          '5.0.1'
+version          '5.0.2'
 chef_version     '>= 12.5' if respond_to?(:chef_version)
 
 issues_url 'https://github.com/evertrue/exhibitor-cookbook/issues' if respond_to?(:issues_url)
